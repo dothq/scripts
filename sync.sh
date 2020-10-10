5808c563ea3748b5e62660b290ad3382d198c716
EnderDev <xenderpugx@gmail.com>
2020-10-10 20:57:37 +0100

    ✨ Add sync script (wip)

--- sync.sh
+++ sync.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 cd browser/dot
-find . -name '*' -exec cp -r --parents \{\} ../firefox \;
+
+copy() {
+    echo "Syncing $1..."
+    \cp -r "$1" "../firefox/$1"
+}
+
+for f in $(find . -not \( -path ./.git -prune \) -not \( -path ./.hg -prune \) -name '*'); do copy $f; done
