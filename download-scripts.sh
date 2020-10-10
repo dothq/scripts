204061137ca0d433958d5325e881c0d79796648e
EnderDev <xenderpugx@gmail.com>
2020-10-10 17:04:05 +0100

    ✨ Remove download-scripts.sh when cloning scripts repo

--- download-scripts.sh
+++ download-scripts.sh
@@ -3,4 +3,11 @@
 mkdir browser > /dev/null 2>&1
 cd browser
 
-git clone https://github.com/dothq/scripts tools && echo -e "\n\x1b[1;32mSUCCESS\x1b[0m Downloaded the build helper scripts.\n" || echo -e "\n\x1b[1;31mERROR\x1b[0m Failed to download build helper scripts.\n"
+post_git_clone() {
+    echo -e "\n\x1b[1;32mSUCCESS\x1b[0m Downloaded the build helper scripts.\n"
+
+    # remove self
+    rm $(pwd)/tools/download-scripts.sh
+}
+
+git clone https://github.com/dothq/scripts tools && post_git_clone || echo -e "\n\x1b[1;31mERROR\x1b[0m Failed to download build helper scripts.\n"
